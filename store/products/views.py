70ed847705c3931fd9fcc8fb351a217d2eabffe2
sepideh <sepideh.hayati16@Gmail.com>
2024-07-17 14:22:43 +0330

    Operation Crad

--- store/products/views.py
+++ store/products/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from . import models
+
 def crad(request):
-    return render(request,'index.html')
+    '''
+    products1 = models.Product(name = 'Sunscreen', price = '800', number = '200')
+    products1.save()
+    '''
+    """
+    products3 = models.Product.objects.filter(id=3).first()
+    products3.name = 'water supply'
+    products3.save()
+    """
+    products2 = models.Product.objects.all()
+
+    products3 = models.Product.objects.filter(id=7).first()
+    products3.delete()
+    return render(request,'index.html',context={'products2':products2})
 
 # Create your views here.
